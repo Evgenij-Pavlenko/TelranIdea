992b5cca1e01fad56b54e922b5b16e4b1c023427
Evgenij <evgenij.pavlenko@gmail.com>
2020-03-13 19:50:01 +0100

    add chekPassword

--- 11_03_2020_User/src/telran/controller/UserApp.java
+++ 11_03_2020_User/src/telran/controller/UserApp.java
@@ -19,32 +19,53 @@ public class UserApp {
         User[] users = {user1, user2, user3, user4, user5};
         String nameUser = getNameFromUser();
 
-        userCollaboration(users,nameUser);
+        userCollaboration(users, nameUser);
 
         scanner.close();
     }
 
     public static String getNameFromUser() {
         System.out.println("Please, enter you name");
-        String userName = scanner.nextLine();
-        return userName;
+        return scanner.nextLine();
     }
 
     public static int operationChoice() {
         System.out.println("If you want get your secret code, enter 1");
         System.out.println("If you want change your name, enter 2");
-        int operation = scanner.nextInt();
-        return operation;
+        return scanner.nextInt();
     }
 
     public static void updateUser(User user) {  //проверка по коду юсера
-        System.out.println("Please, enter your name");
+        System.out.println("Please, enter your new name");
         String newName = scanner.next();
-        user.setName(newName);
-        System.out.println("Your new name is " + user.getName() + " now.");
+        if (checkPassword(user)) {
+            user.setName(newName);
+            System.out.println("Your new name is " + user.getName() + " now.");
+        } else
+            System.out.println("You make nothing");
     }
 
-    public static void makeUserOperationChpise(int operation, User user) {
+    public static boolean checkPassword(User user) {
+        while (true) {
+            System.out.println("Enter you password or 'quit'");
+            String answer = scanner.next();
+            System.out.println("You enter is: " + answer);
+            if (answer.equalsIgnoreCase("quit")) {
+                System.out.println("You make exit");
+                return false;
+            } else if (answer.equalsIgnoreCase(String.valueOf(user.getSecretCode()))) {
+                System.out.println("Secretcode is right");
+                return true;
+            } else {
+                System.out.println("Password is wrong");
+                System.out.println("Enter right password");
+            }
+
+        }
+
+    }
+
+    public static void makeUserOperationChoise(int operation, User user) {
         switch (operation) {
             case 1:
                 System.out.println(user.getSecretCode());
@@ -56,13 +77,14 @@ public class UserApp {
                 System.out.println("Wrong operation number");
         }
     }
-    public static void userCollaboration(User[] users, String name){
-        if (getUserByName(users, name) == null){
+
+    public static void userCollaboration(User[] users, String name) {
+        if (getUserByName(users, name) == null) {
             System.out.println("Sorry, we don't know you");
-        } else{
-            System.out.println("Hello dear "+ name + "!");
+        } else {
+            System.out.println("Hello dear " + name + "!");
             int operation = operationChoice();
-            makeUserOperationChpise(operation, getUserByName(users, name));
+            makeUserOperationChoise(operation, getUserByName(users, name));
         }
     }
 }
