8bfafc87d0ac249b518b614806852e34ae7efe7a
Evgenij <evgenij.pavlenko@gmail.com>
2020-03-16 23:59:27 +0100

    Enum Shedule

--- 16_03_2020_Enum/src/telran/controller/Main.java
+++ 16_03_2020_Enum/src/telran/controller/Main.java
@@ -1,4 +1,24 @@
 package telran.controller;
 
+import telran.data.DayOfWeek;
+import telran.data.Shedule;
+
+import java.util.Arrays;
+
 public class Main {
+    public static void main(String[] args) {
+        DayOfWeek monday = DayOfWeek.MONDAY;
+        DayOfWeek sunday = DayOfWeek.SUNDAY;
+        System.out.println(monday);
+        System.out.println(sunday.ordinal());
+        monday.printHello();
+        System.out.println(monday.getTitle().replace("mo", "Mo"));
+        System.out.println(monday.getTitle());
+        System.out.println(Arrays.toString(DayOfWeek.values()));
+
+        Shedule shedule = new Shedule();
+        shedule.setDayOfWeek(DayOfWeek.MONDAY);
+        shedule.wakeUp();
+        shedule.getMenu(sunday);
+    }
 }
--- 16_03_2020_Enum/src/telran/data/DayOfWeek.java
+++ 16_03_2020_Enum/src/telran/data/DayOfWeek.java
@@ -1,4 +1,33 @@
 package telran.data;
 
 public enum DayOfWeek {
+    MONDAY ("mon"),
+    TUESDAY ("tue"),
+    WEDNESDAY ("wed"),
+    THUSDAY ("thu"),
+    FRIDAY ("fri"),
+    SATURDAY("sat"),
+    SUNDAY ("sun");
+    private String title;
+
+    DayOfWeek(String title) {
+        this.title = title;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public void printHello(){
+        System.out.println("Hello!");
+    }
+
+    @Override
+    public String toString() {
+        return this.name() + ", " + title + ". ";
+    }
 }
