007f0d443a19507412086e928559840f6a9920ff
Evgenij <evgenij.pavlenko@gmail.com>
2020-03-19 20:02:52 +0100

    date right

--- HotelBooking/src/com/company/Date.java
+++ HotelBooking/src/com/company/Date.java
@@ -166,26 +166,3 @@ public class Date {
     //   0 1 2 3 4 5 6 7 8 9 10 11
 }
 
-package com.company;
-
-public class Date {
-    int i1;
-    int i2;
-    int i3;
-
-    public Date(int i1, int i2, int i3) {
-        this.i1 = i1;
-        this.i2 = i2;
-        this.i3 = i3;
-    }
-
-    @Override
-    public String toString() {
-        return "Date{" +
-                "i1=" + i1 +
-                ", i2=" + i2 +
-                ", i3=" + i3 +
-                '}';
-    }
-}
-
