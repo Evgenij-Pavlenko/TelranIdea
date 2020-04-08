48f776c6dcbc2d6039b7d2f0824f96c1505e6e8b
Ievgenij_Pavlenko <evgenij.pavlenko@gmail.com>
2020-04-08 15:39:55 +0200

    add class WorgTransformerChild

--- Telran_HomeWork/src/telran20200407/WordTransformerTest.java
+++ Telran_HomeWork/src/telran20200407/WordTransformerTest.java
@@ -15,6 +15,7 @@ public class WordTransformerTest {
         String test = WordTransformer.transform("aaa bcDE eklm iii");
         assertEquals("AAA bcDE eklm III", test);
     }
+
     /**
      * transform("AAA bc eklm iii") -> "AAA bc eklm III"
      */
@@ -23,6 +24,22 @@ public class WordTransformerTest {
 
         String test = WordTransformer.transform("AAA bc eklm iii");
         assertEquals("AAA bc eklm III", test);
+    }
+    /**
+     * transform4("aAa bcDE ekLm Iii") -> "AAA bcDE ekLm III"
+     */
+    @Test
+    public void testWordTransformerChild(){
+        String test = WorgTransformerChild.transform4("aAa bcDE ekLm Iii", true);
+        assertEquals("AAA bcDE ekLm III", test);
+    }
+/**
+     * transform4("aAa bcDE ekLm Iii") -> "aAa bcde eklm Iii"
+     */
+    @Test
+    public void testWordTransformerChild1(){
+        String test = WorgTransformerChild.transform4("aAa bcDE ekLm Iii", false);
+        assertEquals("aAa bcde eklm Iii", test);
     }
 
 }
