8f52d285febe99edc6e9f736712de0c1dcda9b25
Ievgenij_Pavlenko <evgenij.pavlenko@gmail.com>
2020-04-09 15:16:45 +0200

    modified WordTransfotmerNew 2

--- Telran_HomeWork/src/telran20200407/WordTransformerNew.java
+++ Telran_HomeWork/src/telran20200407/WordTransformerNew.java
@@ -2,56 +2,80 @@ package telran20200407;
 
 public class WordTransformerNew {
 
-    public static String transform(String input) {
+
+    //transform("aaa bcDE eklm iii") -> "AAA bcDE eklm III"
+    //length == 3 -> to UpperCase
+    public static String transform3Letter(String input) {
         String temp = "";
         String[] str = input.split(" ");
-        for (int i = 0; i < str.length; i++) {
-            if (str[i].length() == 3) {
-                temp += str[i].toUpperCase() + " ";
+        for (String s : str) {
+            if (s.length() == 3) {
+                temp += s.toUpperCase() + " ";
             } else {
-                temp += str[i] + " ";
+                temp += s + " ";
             }
         }
         return temp.trim();
     }
 
-    public static String transform(String input, int param) {
-        switch (param) {
-            //transform("aaa bcDE eklm iii") -> "AAA bcDE eklm III"
-            //length == 3 -> to UpperCase
-            case 3:
-                return transform(input);
-            //transform("aaa bcDE ekLm iii") -> "aaa bcde eklm iii"
-            //length == 4 -> to LowerCase
-            case 4:
-                String temp = "";
-                String[] str = input.split(" ");
-                for (int i = 0; i < str.length; i++) {
-                    if (str[i].length() == 4) {
-                        temp += str[i].toLowerCase() + " ";
-                    } else {
-                        temp += str[i] + " ";
-                    }
-                }
-                return temp.trim();
-            default:
-                return "Param is wrong";
+    //transform("aaa bcDE ekLm iii") -> "aaa bcde eklm iii"
+    //length == 4 -> to LowerCase
+    public static String transform4Letter(String input) {
+        String temp = "";
+        String[] str = input.split(" ");
+        for (String s : str) {
+            if (s.length() == 4) {
+                temp += s.toLowerCase() + " ";
+            } else {
+                temp += s + " ";
+            }
         }
+        return temp.trim();
+
     }
 
     //этот же метод заменял слова, начинающиеся на 'a' на звездочку. То есть 'aBcdE' -> '*****'
-    public static String transform(String input, String start) { // kann char make
-//        public static String transform(String input, int param, String start) - непонятно - тут нужен параметр, который делает toUpperCase?
+    public static String transformStar(String input, String start) { // kann char make
         String[] str = input.split(" ");
         String temp = "";
-        for (int i = 0; i < str.length; i++) {
+        for (String s : str) {
             //Kann with str.IgnoreCase make
-            if (str[i].startsWith(start)) {
-                temp += str[i].replaceAll(".", "*") + " ";
+            if (s.startsWith(start)) {
+                temp += s.replaceAll(".", "*") + " ";
             } else {
-                temp += str[i] + " ";
+                temp += s + " ";
             }
         }
         return temp.trim();
+    }
+
+    public static String transform(String input){
+        return transform3Letter(input);
+    }
+    public static String transform(String input, int param) {
+        if (param == 3) {
+            return transform3Letter(input);
+        } else if (param == 4) {
+            return transform4Letter(input);
+        }
+        return "Wrong Param";
+    }
+
+    public static String transform(String input, String start) {
+        return transformStar(input, start);
+    }
+
+    // universal method
+    public static String transform(String input, int param, String start) {
+        switch (param) {
+            case 0:
+                return transformStar(input, start);
+            case 3:
+                return transform3Letter(input);
+            case 4:
+                return transform4Letter(input);
+            default:
+                return "Wrong Param";
+        }
     }
 }
