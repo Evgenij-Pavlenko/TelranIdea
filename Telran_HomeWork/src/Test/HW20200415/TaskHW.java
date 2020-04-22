c1ea022ba52c381fbde7ba3f222430407466de59
Ievgenij_Pavlenko <evgenij.pavlenko@gmail.com>
2020-04-22 21:22:52 +0200

    isStringIsMutationEachOther2- 3rd time -shorter

--- Telran_HomeWork/src/Test/HW20200415/TaskHW.java
+++ Telran_HomeWork/src/Test/HW20200415/TaskHW.java
@@ -18,27 +18,20 @@ public class TaskHW {
 
     public static boolean isStringIsMutationEachOther2(String s1, String s2) {
         if (((s1 != null) && (s2 != null)) && Math.abs((s1.length() - s2.length())) == 1) {
-            char[] ch1 = s1.toCharArray();
-            char[] ch2 = s2.toCharArray();
+            char[] ch1;
+            char[] ch2;
+            if (s1.length() > s2.length()) {
+                ch1 = s2.toCharArray();
+                ch2 = s1.toCharArray();
+            } else{
+                ch2 = s2.toCharArray();
+                ch1 = s1.toCharArray();
+            }
             Arrays.sort(ch1);
             Arrays.sort(ch2);
             int count = 0;
             // бред какойто. Если использовать цикл в цикле - непонятно, что делать с двойной буквой
             //есть какой-то метод у String еще? типа StringUtils.containsOnly()
-            if (ch1.length > ch2.length) {
-                for (int i = 0; i < ch2.length; i++) {
-                    if (ch2[i] == ch1[i]) {
-                        count++;
-                    }else{
-                        if (ch2[i]==ch1[i+1]){
-                            count++;
-                        }
-                    }
-                    if (count==ch2.length-1){
-                        return true;
-                    }
-                }
-            } else {
                 for (int i = 0; i < ch1.length; i++) {
                     if (ch1[i] == ch2[i]) {
                         count++;
@@ -50,7 +43,7 @@ public class TaskHW {
                     if (count==ch1.length-1){
                         return true;
                     }
-                }
+
             }
 
         }
