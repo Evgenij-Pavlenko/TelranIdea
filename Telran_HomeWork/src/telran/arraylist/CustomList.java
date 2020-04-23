f491e4ab2ae057570c07fe61616a83bff7e8101d
Ievgenij_Pavlenko <evgenij.pavlenko@gmail.com>
2020-04-23 23:57:00 +0200

    ArrayList HW -Start (in the class)

---
+++ Telran_HomeWork/src/telran/arraylist/CustomList.java
@@ -0,0 +1,13 @@
+package telran.arraylist;
+
+public interface CustomList<E> {
+    int size();
+
+    void append(E o);
+
+    E get(int index);
+
+    E removeById(int index);
+
+    boolean remove(E e);
+}
