55a9708bea3a8628d2d47994b7489cac18b0e3df
Ievgenij_Pavlenko <evgenij.pavlenko@gmail.com>
2020-04-27 23:28:26 +0200

    Linkedlist-Lesson

---
+++ Telran_HomeWork/src/telran/Linkedlist20200427/CustomLinkedList.java
@@ -0,0 +1,69 @@
+package telran.Linkedlist20200427;
+
+public class CustomLinkedList<E> implements CustomList<E> {
+
+    private Node<E> first;
+    private Node<E> last;
+    private int size;
+
+    @Override
+    public int size() {
+        return size;
+    }
+
+    @Override
+    public void append(E o) {
+
+        if (size > 0) {
+            Node newNode = new Node(null, last, o);
+            last.next = newNode;
+            last = newNode;
+        } else {
+            Node newNode = new Node(null, null, o);
+            first = newNode;
+            last = newNode;
+        }
+        size++;
+    }
+
+    @Override
+    public E get(int index) {
+        if (index >= size || index < 0) {
+            throw new IndexOutOfBoundsException();
+        }
+
+        Node ret = getNode(index);
+        return (E) ret.value;
+    }
+
+    private Node getNode(int index) {
+        Node ret = first;
+        for (int i = 0; i < index; i++) {
+            ret = ret.next;
+        }
+        return ret;
+    }
+
+    @Override
+    public E removeById(int index) {
+        return null;
+    }
+
+    @Override
+    public boolean remove(E o) {
+        return false;
+    }
+
+    private static class Node<E> {
+        E value;
+        Node next;
+        Node prev;
+
+
+        public Node(Node next, Node prev, E value) {
+            this.next = next;
+            this.prev = prev;
+            this.value = value;
+        }
+    }
+}
