c51ed7f96d7e292041b54138679d297b1737a7ac
Ievgenij_Pavlenko <evgenij.pavlenko@gmail.com>
2020-06-26 00:10:41 +0200

    correct PREVIEW

--- ImageProcessorMaven/src/main/java/de/telran/action/PreviewImageAction.java
+++ ImageProcessorMaven/src/main/java/de/telran/action/PreviewImageAction.java
@@ -7,7 +7,7 @@ public class PreviewImageAction implements ImageAction {
     @Override
     public BufferedImage doAction(BufferedImage source) {
         System.out.println("Creating a preview");
-        return null;
+        return doResizedPreview(source);
     }
 
     private BufferedImage doResizedPreview(BufferedImage source) {
