8a6aa9e1e32ed0a82d777a4cf8d5eb442991112b
Ievgenij_Pavlenko <evgenij.pavlenko@gmail.com>
2020-07-04 18:01:25 +0200

    add files name. test - 1 wrong

---
+++ ImageProcessorMaven/src/main/java/de/telran/ImageProcessor.java
@@ -0,0 +1,70 @@
+package de.telran;
+
+import de.telran.entity.ActionableImage;
+import de.telran.entity.ImageDescriptor;
+import de.telran.factory.ImageActionFactory;
+import de.telran.service.*;
+
+import java.awt.image.BufferedImage;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class ImageProcessor {
+    private ImageDescriptorService imageDescriptorService;
+    private DownloadService downloadService;
+    private ImageService imageService;
+    private FileService fileService;
+
+    public ImageProcessor(ImageDescriptorService imageDescriptorService,
+                          DownloadService downloadService,
+                          ImageService imageService,
+                          FileService fileService) {
+        this.imageDescriptorService = imageDescriptorService;
+        this.downloadService = downloadService;
+        this.imageService = imageService;
+        this.fileService = fileService;
+    }
+
+    public void doProcessing(String fileName) {
+
+        List<ImageDescriptor> imageDescriptors = imageDescriptorService.getImageDescriptors(fileName);
+
+        List<ActionableImage> actionableImages = imageDescriptors
+                .stream()
+                .map(i -> new ActionableImage(null, false, i.getImageUrlName(), i.getActionName()))
+                .collect(Collectors.toList());
+
+        List<ActionableImage> downloadedImages = downloadService.downloadImages(actionableImages);
+
+        List<ActionableImage> successfullyDownloadedImages = downloadedImages.stream()
+                .filter(ActionableImage::isSuccessfull)
+                .collect(Collectors.toList());
+
+        List<ActionableImage> processedImages = successfullyDownloadedImages
+                .stream()
+                .map(i -> imageService.processImage(i))
+                .collect(Collectors.toList());
+
+        processedImages.forEach(i -> fileService.saveImageAsFile(i));
+
+    }
+
+
+    public static void main(String[] args) throws Exception {
+
+        String fileName = args[0];
+
+        FileService fileService = new FileService(new ConfigService());
+
+        ImageDescriptorService imageDescriptorService = new ImageDescriptorService(fileService);
+
+        DownloadService downloadService = new DownloadService();
+
+        ImageService imageService = new ImageService(new ImageActionFactory(new ActionsConfigService()));
+
+        ImageProcessor processor = new ImageProcessor(imageDescriptorService, downloadService, imageService, fileService);
+
+        processor.doProcessing(fileName);
+
+    }
+}
